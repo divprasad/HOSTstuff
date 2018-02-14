6476781562ecd7673333774fd568f1902eda1c4b
divprasad <dkp.iitb@gmail.com>
2018-02-14 20:05:35 +0100

    edits

--- UniqueIdentifiers/copysingle.py
+++ UniqueIdentifiers/copysingle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import shutil
 
-with open ('prophage_BOP3.tsv', 'r') as f:
+with open ('VS.host.toedit.txt', 'r') as f:
 	for line in f:
 		filename = line.rstrip()
 		filename = filename + ".fna"
 		try:
-			shutil.copy2(filename,'/home/divyae/divyae2/HOSTS/UniqueIdentifiers/SmallerCleanerHosts/')
+			shutil.copy2(filename,'/home/divyae/divyae2/HOSTS/UniqueIdentifiers/VShosts')
                 except IOError:
-			thefile = open('notfound.txt', "a")
-			item=filename+"__NOTFOUND" 
+			thefile = open('notfound_VS.txt', "a")
+			item=filename		#+"__NOTFOUND" 
 			print>>thefile, item
 			thefile.close()
 
