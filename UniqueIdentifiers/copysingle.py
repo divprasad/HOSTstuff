5a56dc303bc871f96d8e8cbcb6de21cf8759687c
divprasad <dkp.iitb@gmail.com>
2018-02-16 19:16:42 +0100

    clearing junk

--- UniqueIdentifiers/copysingle.py
+++ UniqueIdentifiers/copysingle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import shutil
 
-with open ('VS.host.toedit.txt', 'r') as f:
+with open ('proper.inPat_60FULLPATH.txt', 'r') as f:
 	for line in f:
 		filename = line.rstrip()
-		filename = filename + ".fna"
+		filename = filename
 		try:
-			shutil.copy2(filename,'/home/divyae/divyae2/HOSTS/UniqueIdentifiers/VShosts')
+			shutil.copy2(filename,'/home/divyae/divyae2/HOSTS/UniqueIdentifiers/16Feb')
                 except IOError:
-			thefile = open('notfound_VS.txt', "a")
+			thefile = open('notfound_16Feb.txt', "a")
 			item=filename		#+"__NOTFOUND" 
 			print>>thefile, item
 			thefile.close()
