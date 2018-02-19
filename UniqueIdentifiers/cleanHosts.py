fdf7b57e5d1521c710b5f852c1103331f70edb78
divprasad_Dell <dkp.iitb@gmail.com>
2018-02-19 23:42:58 +0100

    yusss

--- UniqueIdentifiers/cleanHosts.py
+++ UniqueIdentifiers/cleanHosts.py
@@ -14,7 +14,7 @@ import os, sys
 import os.path
 sidwords=[]
 
-fedit="prophage_BOP.tsv"      #just an extra precaution - force it to string type
+fedit="topHit.VS_16Feb_BOP.tsv"      #just an extra precaution - force it to string type
 i=1
 f2= open("changedSomeMuch.txt", "a")
 with open(fedit, "r") as f:
@@ -28,28 +28,28 @@ with open(fedit, "r") as f:
             temp=start
             start=end
             end=temp
-        file3="mod_"+file1
+        file3="edit_"+file1
         f1= open(file3, "a")
         for seq_record in SeqIO.parse(file1, "fasta"):  #iterates through all the sequences in each file
              file2=file1.split('.f')[0]                 #so that we dont write the file extension at L(24)
              sequence=seq_record.seq
              sid=seq_record.id
-             sid=sid.split('**')[0].split('@@')[0]
+             sid=sid.split('***')[0].split('@@@')[0]
              if sid == contigID:
                 first=sequence[0:start-1]
                 middle="N"*(end-start)
                 last=sequence[end:]
                 sequence=first+middle+last
-                #sidwords=seq_record.id.split('@@')
+                #sidwords=seq_record.id.split('@@@')
                 #if len(sidwords)==1:
                 del sidwords[:]
-                sidwords=seq_record.id.split('**')
+                sidwords=seq_record.id.split('***')
                 if len(sidwords)==1:
-                    sidwords[0]=sid+'@@'+file2
+                    sidwords[0]=sid+'@@@'+file2
                     sidwords.append(1)
                 else:
                     sidwords[1]=int(sidwords[1])+1
-                sid=('**'.join(str(v) for v in sidwords))
+                sid=('***'.join(str(v) for v in sidwords))
              print>>f1,">"+sid
              print>>f1, sequence
              if seq_record.id != sid:
